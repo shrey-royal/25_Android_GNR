ccee57dbc72d068c85264085c7cc2a4dc0956c54
Shrey K <srkkadia@gmail.com>
2025-09-30 18:41:20 +0530

    Linear Layout, Nested Layouts

---
+++ Pro0/app/src/main/java/com/royal/pro0/Layouts.java
@@ -0,0 +1,14 @@
+package com.royal.pro0;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Layouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+    }
+}
